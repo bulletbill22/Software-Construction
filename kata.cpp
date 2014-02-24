d87b6f42fd34b7c7aac45c85899f0da8341a4c7a
bulletbill22 <choch@alaska.edu>
2014-02-24 14:37:07 -0900

    Added function "convert" in module, added first test in kata, test fails

--- kata.cpp
+++ kata.cpp
@@ -1,7 +1,10 @@
 #include "module.h"
+#include <iostream>
+using std::cout;
+using std::endl;
 
 int main()
 {
-	
+	if (!(convert(1)=="I")) cout << "convert(1)==\"I\"" << endl;
 	return 0;
 }
--- module.h
+++ module.h
@@ -1,6 +1,12 @@
 #ifndef MODULE_H_INCLUDED
 #define MODULE_H_INCLUDED
 
+#include <string>
+using std::string;
 
+string convert(const int & roman)
+{
+	return "";
+}
 
 #endif
