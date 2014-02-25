4c2a3425e1cb0c2c433dc4f3f4784e7378903717
bulletbill22 <choch@alaska.edu>
2014-02-24 16:28:10 -0900

    Refactored tests to use a testing function, a set of test cases, and a loop through test cases.

--- kata.cpp
+++ kata.cpp
@@ -2,21 +2,42 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+#include <string>
+using std::string;
+#include <map>
+using std::map;
+
+map<int, string> test_cases = {
+	{2, "II"},
+	{5, "V"},
+	{7, "VII"},
+	{10, "X"},
+	{13, "XIII"},
+	{15, "XV"},
+	{16, "XVI"},
+	{22, "XXII"},
+	{25, "XXV"},
+	{27, "XXVII"},
+	{30, "XXX"},
+	{31, "XXXI"},
+	{38, "XXXVIII"}
+};
+
+
+bool test(int convert_paramater, string desired_output)
+{
+	return convert(convert_paramater)==desired_output;
+}
 
 int main()
 {
-	if(!(convert(2)=="II"))      cout<<"convert(2)=" <<convert(2) <<endl;
-	if(!(convert(5)=="V"))       cout<<"convert(3)=" <<convert(3) <<endl;
-	if(!(convert(7)=="VII"))     cout<<"convert(7)=" <<convert(7) <<endl;
-	if(!(convert(10)=="X"))      cout<<"convert(10)="<<convert(10)<<endl;
-	if(!(convert(13)=="XIII"))   cout<<"convert(13)="<<convert(13)<<endl;
-	if(!(convert(15)=="XV"))     cout<<"convert(15)="<<convert(15)<<endl;
-	if(!(convert(16)=="XVI"))    cout<<"convert(16)="<<convert(16)<<endl;
-	if(!(convert(22)=="XXII"))   cout<<"convert(22)="<<convert(22)<<endl;
-	if(!(convert(25)=="XXV"))    cout<<"convert(25)="<<convert(25)<<endl;
-	if(!(convert(27)=="XXVII"))  cout<<"convert(27)="<<convert(27)<<endl;
-	if(!(convert(30)=="XXX"))    cout<<"convert(30)="<<convert(30)<<endl;
-	if(!(convert(31)=="XXXI"))   cout<<"convert(31)="<<convert(31)<<endl;
-	if(!(convert(38)=="XXXVIII"))cout<<"convert(38)="<<convert(38)<<endl;
+	for(auto test_case : test_cases)
+	{
+		if (!test(test_case.first, test_case.second))
+		{
+			cout << "Error: tried converting " << test_case.first << " to " << test_case.second << ". Actual result: " << convert(test_case.first);
+			return 1;
+		}
+	}
 	return 0;
 }
